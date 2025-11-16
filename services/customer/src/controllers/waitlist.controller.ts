7caa4d5e86f1693cdbba31d1d03c028c505356a9
Rob Weinstein <robweinstein@example.com>
2025-11-15 17:40:05 -0700

    fix: use default import for AppError

--- services/customer/src/controllers/waitlist.controller.ts
+++ services/customer/src/controllers/waitlist.controller.ts
@@ -10,7 +10,7 @@
 
 import { Request, Response, NextFunction } from 'express';
 import { PrismaClient } from '@prisma/client';
-import { AppError } from '../utils/appError';
+import AppError from '../utils/appError';
 
 const prisma = new PrismaClient();
 
